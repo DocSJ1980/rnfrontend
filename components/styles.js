2030c6390cd6facc304ec99b4bc54d3dfe2876b6
DocSJ1980 <docsaj@outlook.com>
2022-08-05 12:16:30 -0700

    Login Page Complete

--- components/styles.js
+++ components/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, TextInput, TouchableOpacity } from "react-native";
 import Constants from "expo-constants";
 
 const StatusBarHeight = Constants.statusBarHeight;
@@ -30,23 +30,126 @@ export const InnerContainer = styled.View`
   align-items: center;
 `;
 export const PageLogo = styled.Image`
-  width: 250px;
-  height: 250px;
+  width: 200px;
+  height: 200px;
 `;
 
 export const PageTitle = styled.Text`
-  font-size: 35px;
+  font-size: 25px;
   text-align: center;
   font-weight: bold;
-  color: ${Colors.brand};
-  padding: 10px;
-  margin-bottom: 20px;
+  color: ${brand};
 `;
 
 export const SubTitle = styled.Text`
   font-size: 13px;
   text-align: center;
-  color: ${Colors.brand};
-  padding: 10px;
+  color: ${brand};
+  padding: 5px;
   margin-bottom: 20px;
+  margin-vertical: 10px;
+`;
+
+export const StyledFormArea = styled.View`
+  width: 90%;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  background-color: ${secondary};
+  padding-left: 55px;
+  border-radius: 5px;
+  font-size:16px;
+  height: 60px;
+  margin-bottom: 10px;
+  color: ${tertiary};
+`;
+
+export const StyledTextInputLabel = styled.Text`
+  font-size: 13px;
+  text-align: left;
+  color: ${brand};   
+  padding: 10px;
+`;
+
+export const LeftIcon = styled.View`
+  left: 15px;
+  top: 53px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+  right: 15px;
+  top: 53px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${brand};
+  justify-content: center;
+  align-items: center;
+  border-radius: 45px;
+  margin-vertical: 25px;
+  margin-left: 75px;
+  height: 60px;
+  width: 150px;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${primary};
+  font-size: 16px;
+`;
+
+export const MsgBox = styled.Text`
+  text-align: center;
+  font-size: 13px;
+`;
+
+export const Line = styled.View`
+  height: 1px;
+  width: 100%;
+  background-color: ${darkLight};
+  margin-vertical: 10px;
+`;
+
+export const ExtraView = styled.View`
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const ExtraText = styled.Text`
+  justify-content: center;
+  align-content: center;
+  color: ${tertiary};
+  font-size: 15px;
+`;
+
+export const TextLink = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextLinkContent = styled.Text`
+  color: ${brand};
+  font-size: 15px;
+`;
+
+export const StyledButtonSmall = styled.TouchableOpacity`
+  padding: 1px;
+  background-color: ${brand};
+  justify-content: center;
+  align-items: center;
+  margin-left: 5px;
+  border-radius: 45px;
+  height: 25px;
+  width: 100px;
+`;
+
+export const SmallButtonText = styled.Text`
+  color: ${primary};
+  font-size: 10px;
 `;
--- screens/Login.js
+++ screens/Login.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import { StatusBar } from 'expo-status-bar';
+import { TextInput, View, Text, Linking, Platform } from 'react-native'
 
 
 import {
@@ -7,19 +8,118 @@ import {
     InnerContainer,
     PageLogo,
     PageTitle,
-    SubTitle
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    RightIcon,
+    StyledTextInputLabel,
+    StyledTextInput,
+    Colors,
+    StyledButton,
+    ButtonText,
+    Line,
+    MsgBox,
+    ExtraView,
+    ExtraText,
+    TextLink,
+    TextLinkContent,
+    SmallButtonText,
+    StyledButtonSmall
 } from "./../components/styles.js"
 
+import { Formik } from "formik";
+import { Octicons } from '@expo/vector-icons'
+const { primary, secondary, tertiary, darkLight, brand, green, red } = Colors;
+const dialCall = () => {
+
+    let phoneNumber = '';
+
+    if (Platform.OS === 'android') {
+        phoneNumber = 'tel:${1234567890}';
+    }
+    else {
+        phoneNumber = 'telprompt:${1234567890}';
+    }
+
+    Linking.openURL(phoneNumber);
+};
+
 const Login = () => {
+    const [hidePassword, setHidePassword] = useState(true);
     return (
         <StyledContainer>
             <StatusBar style="dark" />
             <InnerContainer>
                 <PageLogo resizeMode="cover" source={require("./../assets/img/img1.png")} />
                 <PageTitle>District Health Authority Rawalpindi</PageTitle>
+                <SubTitle>Account Login</SubTitle>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values }) => (
+                        <StyledFormArea>
+                            <MyTextInput
+                                label="Email Address" icon="mail"
+                                placeholder="address@example.com"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                                keyboardType="email-address"
+                            />
+                            <MyTextInput
+                                label="Password" icon="lock"
+                                placeholder="* * * * * * * *"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                                secureTextEntry={hidePassword}
+                                isPassword={true}
+                                hidePassword={hidePassword}
+                                setHidePassword={setHidePassword}
+                            />
+                            <MsgBox>...</MsgBox>
+                            <StyledButton onPress={handleSubmit}>
+                                <ButtonText>Login</ButtonText>
+                            </StyledButton>
+                            <Line />
+                            <ExtraView>
+                                <ExtraText>Don't have an account?</ExtraText>
+                                <StyledButtonSmall onPress={dialCall}>
+                                    <SmallButtonText>Call Control Room</SmallButtonText>
+                                </StyledButtonSmall>
+                            </ExtraView>
+                        </StyledFormArea>
+                    )}
+                </Formik>
                 <SubTitle>Powered by Epidemics Prevention & Control Cell</SubTitle>
             </InnerContainer>
         </StyledContainer>
+    )
+}
+
+const MyTextInput = ({ label, icon, hidePassword, setHidePassword, isPassword, ...props }) => {
+    return (
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={brand} />
+            </LeftIcon>
+            <StyledTextInputLabel>{label}</StyledTextInputLabel>
+            <StyledTextInput {...props} />
+            {isPassword && (
+                <RightIcon
+                    onPress={() => {
+                        setHidePassword(!hidePassword);
+                    }}
+                >
+                    <Octicons name={hidePassword ? 'eye-closed' : 'eye'} size={30} color={darkLight} />
+                </RightIcon>
+            )}
+        </View>
     )
 }
 

c482a74b0cc38db10934b7ffec6c37cffb8edd98
DocSJ1980 <docsaj@outlook.com>
2022-08-10 07:12:16 -0700

    Login + async storage of token implemented

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -4,39 +4,54 @@ import axios from "axios";
 export const AuthContext = createContext({})
 
 export const AuthProvider = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
     const [userToken, setUserToken] = useState(null)
-
+    const [userInfo, setUserInfo] = useState(null)
     const login = (email, password) => {
-        setIsLoading(false)
+        setIsLoading(true)
+        console.log(isLoading)
         axios.post('http://auth.sjnotes.tk:5001/api/auth/login', {
             email,
             password
         })
-            .then(res => {
-                console.log(res.data)
+            .then(async (res) => {
+                try {
+                    setUserInfo(res.data.message)
+                    setUserToken(res.data.token)
+                    await AsyncStorage.setItem('userTokenLocal', res.data.token)
+                    await AsyncStorage.setItem('userInfoLocal', JSON.stringify(userInfo))
+                    console.log("Token: ", userToken, "Message: ", userInfo)
+                } catch (error) {
+                    `error hai ${error}`
+                }
             })
-            .catch(error => {
-                console.log(`API Call error ${error}`)
+            .catch(e => {
+                console.log(`API Error ${e}`)
             })
-        // setUserToken('jjkljkjgsfaddafkdja')
-        // AsyncStorage.setItem('userToken', "jjkljkjgsfaddafkdja")
         setIsLoading(false)
     }
 
     const logout = () => {
+        console.log("Token: ", userToken, "Message: ", userInfo)
         setIsLoading(true)
-        AsyncStorage.removeItem('userToken')
+        AsyncStorage.removeItem('userTokenLocal')
+        AsyncStorage.removeItem('userInfoLocal')
         setUserToken(null)
+        setUserInfo(null)
         setIsLoading(false)
     }
 
     const isLoggedIn = async () => {
         try {
             setIsLoading(true)
-            let userTokenLocal = await AsyncStorage.getItem('userToken')
-            setUserToken(userTokenLocal)
-            setIsLoading(false)
+            let userTokenLocal = await AsyncStorage.getItem('userTokenLocal')
+            let userInfoLocal = await AsyncStorage.getItem('userInfoLocal')
+            if (userTokenLocal) {
+                setUserToken(userTokenLocal)
+                setUserInfo(userInfoLocal)
+                setIsLoading(false)
+            }
+            console.log(userToken, userInfo)
         } catch (error) {
             console.log(`isLogged in error ${error}`)
         }
